3d8054c4643798b49ed40fb712028d7a0516a96f
AwsmeAnthony <130766283+AwsmeAnthony@users.noreply.github.com>
2025-04-23 15:39:47 -0400

    Added movement

--- ent/player/player.gd
+++ ent/player/player.gd
@@ -3,14 +3,18 @@ extends CharacterBody2D
 @onready var sprite: Sprite2D = $Sprite2D
 @onready var neck: Node2D = $neck
 @onready var emit_partic: CPUParticles2D = $"neck/emit partic"
+@onready var neg: RayCast2D = $neck/neg
+@onready var pos: RayCast2D = $neck/pos
 
 
 func _physics_process(delta: float) -> void:
 	if Input.is_action_pressed("click"):
 		var rot_tween : Tween = create_tween()
 		neck.look_at(get_global_mouse_position())
-		Engine.time_scale = 0.5
+		Engine.time_scale = 0.3
 		emit_partic.emitting = true
 	else:
 		Engine.time_scale = 1
+	
+	velocity = Vector2.RIGHT.rotated(neck.rotation) * 100
 	move_and_slide()
